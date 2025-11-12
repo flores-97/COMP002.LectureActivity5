8987b38b847653170a090912b9de74dc83d7b7fd
flores-97 <floresvictor15@gmail.com>
2025-11-12 15:24:23 -0800

    corrected the for loop. 2 bugs found

--- script1.js
+++ script1.js
@@ -2,12 +2,15 @@ myGrades = [100, 100, 90, 73, 78, 94, 86];
 
 myAverage = 0;
 
-for (let i = 1; i <= myGrades.length; i++) {
-
-myAverage += myGrades[i];
-
-}
+for (let i = 0; i < myGrades.length; i++) {
+    myAverage += myGrades[i];
+    }
 
 myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
+
+/*
+The loop statement was 'let i = 1; i <= myGrades.length; i++' but had to be 'let i = 0; i < myGrades.length; i++' because using the number 1 would skip 
+over the grade "100" that is under the zero value [0]. With the original value the output would be NaN because the conditions would attempt to access values outside the array.  
+*/
